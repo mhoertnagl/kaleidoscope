ab061fcdcb17c2c59425851d310257d429c22583
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2020-09-19 19:18:32 +0200

    lalrpop tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod lexer;
 mod token;
 
+#[macro_use] extern crate lalrpop_util;
+
 //use crate::lexer::Lexer;
 
 fn main() {
@@ -9,4 +11,15 @@ fn main() {
   // for tok in lexer {
   //   println!("{:?}", tok);
   // }
+}
+
+lalrpop_mod!(pub parser); // synthesized by LALRPOP
+
+#[test]
+fn parser() {
+  let exp = 12.0;
+  match parser::ExprParser::new().parse("(1 + 2) * 4") {
+    Ok(v) => assert_eq!(exp, v, "Expecting {} but got {}.", exp, v) ,
+    Err(err) => panic!(err)
+  }
 }
