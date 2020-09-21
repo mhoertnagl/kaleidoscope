2db3dc40f04e1f5df826c7fc856194d820b0057d
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2020-09-21 15:39:28 +0200

    finally figured

--- src/ast.rs
+++ src/ast.rs
@@ -18,7 +18,7 @@ impl fmt::Display for Module {
 pub enum Statement {
   Let {
     name: String,
-    expr: Expr,
+    expr: Box<Expr>,
   },
   Def {
     name: String,
@@ -27,17 +27,20 @@ pub enum Statement {
   },
   Assign {
     name: String,
-    expr: Expr,
+    expr: Box<Expr>,
+  },
+  Expr {
+    expr: Box<Expr>,
   },
   Return {
-    expr: Expr,
+    expr: Box<Expr>,
   }
 }
 
 impl fmt::Display for Statement {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
-      Statement::Let{ name, expr } => write!(f, "let {} = {}", name, expr),
+      Statement::Let{ name, expr } => write!(f, "let {} = {};", name, expr),
       Statement::Def{ name, params, statements } => {
         let ps = params.iter().fold(
           String::new(),
@@ -47,17 +50,22 @@ impl fmt::Display for Statement {
           String::new(),
           |acc, s| acc + &s.to_string() + "\n"
         );
-        write!(f, "def {} ({}) begin {} end", name, ps, ss)
+        write!(f, "def {} ({}) begin \n{}end", name, ps, ss)
       },
-      Statement::Assign{ name, expr } => write!(f, "{} = {}", name, expr),
-      Statement::Return{ expr } => write!(f, "return {}", expr),
+      Statement::Assign{ name, expr } => write!(f, "{} = {};", name, expr),
+      Statement::Expr{ expr } => write!(f, "{};", expr),
+      Statement::Return{ expr } => write!(f, "return {};", expr),
     }
   }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expr {
-  Num(f64),
+  Atom(Atom),
+  FunCall {
+    name: String,
+    args: Vec<Box<Expr>>,
+  },
   BinOp {
     left: Box<Expr>,
     op: Opcode,
@@ -68,8 +76,30 @@ pub enum Expr {
 impl fmt::Display for Expr {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
-      Expr::Num(n) => write!(f, "{}", n),
+      Expr::Atom(a) => write!(f, "{}", a),
+      Expr::FunCall{ name, args } => {
+        let ps = args.iter().fold(
+          String::new(),
+          |acc, s| acc + &s.to_string() + ", "
+        );
+        write!(f, "{}({})", name, ps)
+      },
       Expr::BinOp { left, op, right } => write!(f, "({} {} {})", left, op, right),
+    }
+  }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Atom {
+  Num(f64),
+  Id(String),
+}
+
+impl fmt::Display for Atom {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Atom::Num(n) => write!(f, "{}", n),
+      Atom::Id(id) => write!(f, "{}", id),
     }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,29 @@ mod ast;
 #[macro_use] extern crate lalrpop_util;
 
 fn main() {
-  match parser::ModuleParser::new().parse("let a = 0") {
+  let prog = r"
+    let a = 0;
+    let b = 1;
+    def f(x, y) begin
+      let z = x * y;
+      return z - 3 * x;
+    end
+    h = 3 * (a + f(b, 2));
+    h;
+  ";
+  match parser::ModuleParser::new().parse(prog) {
     Ok(v) => println!("{}", v),
     Err(err) => println!("{}", err),
   }
 }
 
-lalrpop_mod!(pub parser); // synthesized by LALRPOP
+lalrpop_mod!(pub parser);
 
 #[test]
 fn parser() {
-  let exp = 12.0;
-  match parser::ModuleParser::new().parse("(1 + 2) * 4") {
-    Ok(v) => assert_eq!(exp, v, "Expecting {} but got {}.", exp, v) ,
-    Err(err) => panic!(err),
-  }
+  let expr = parser::ModuleParser::new()
+    .parse("(1 + 2) * 4")
+    .unwrap();
+
+  assert_eq!(&format!("{:?}", expr), "((1 + 2) * 4)");
 }
